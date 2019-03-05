d574d4a496ff917e49dbb7ef42f075711f07fc24
MagicalBrain <13542593649@163.com>
2019-03-05 22:38:51 +0800

    更新一下

--- BinarySortTree/BSTree.h
+++ BinarySortTree/BSTree.h
@@ -15,13 +15,37 @@ typedef int Status;
 
 using namespace std;
 
+//二叉查找树结点定义
 struct BSTNode
 {
 	KeyType data;
 	BSTNode *left, *right;
 };
 
+//二叉树查找树定义
 struct BSTree
 {
 	BSTNode *root;
 };
+
+
+
+
+
+bool find(KeyType x, BSTNode *t)
+{
+	if (t == false)
+		return false;
+	else if (x < t->data)
+		return  find(x, t->left);
+	else if (t->data < x)
+		return find(x, t->right);
+	else
+		return false;
+}
+
+
+bool find(KeyType &x)
+{
+	return find(x, root);
+}
